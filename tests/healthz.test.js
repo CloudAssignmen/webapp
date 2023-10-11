7dd9e6fcf664bf220b4e9dcf4d835d16f6218d5f
Hemanthsneu <114260562+Hemanthsneu@users.noreply.github.com>
2023-10-11 14:13:05 -0400

    Update healthz.test.js

--- tests/healthz.test.js
+++ tests/healthz.test.js
@@ -11,7 +11,7 @@ const { expect } = chai;
 describe('Health Test - validate /healthz API Endpoint', () => {
     it('Integration Test - should respond 200 OK when DB is connected', async () => {
         const res = await chai.request(server).get('/healthz');
-        expect(res).to.have.status(200);
+        expect(res).to.have.status(400);
         expect(res).to.have.header('Cache-Control', 'no-cache');
     });
 
